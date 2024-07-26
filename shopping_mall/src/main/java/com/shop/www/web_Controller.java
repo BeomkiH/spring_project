3e7710ded3b904da99f0da9d8bcaf47ad5c24648
BeomkiH <vores@naver.com>
2024-07-26 11:47:33 +0900

    web_controller

--- shopping_mall/src/main/java/com/shop/www/web_Controller.java
+++ shopping_mall/src/main/java/com/shop/www/web_Controller.java
@@ -2,9 +2,15 @@ package com.shop.www;
 
 import java.io.PrintWriter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class web_Controller {
 
 	PrintWriter pw = null;
+	@GetMapping("/ajaxok.do")
+	public String ajaxok() {
+		
+		return null;
+	}
 }
